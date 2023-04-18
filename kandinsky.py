96c49e1d36e335a4bfbb7ee720d71d5ec6dba2a7
MaximKoles <128639334+MaximKoles@users.noreply.github.com>
2023-04-18 17:00:01 +0300

    Update kandinsky.py

--- kandinsky.py
+++ kandinsky.py
@@ -1,43 +1,42 @@
 import os
+import aiogram
+from aiogram import types
+from aiogram.dispatcher import filters
+from aiogram.dispatcher import dispatcher
 import replicate
-from aiogram import Bot, Dispatcher
 
-# Получить токены доступа для бота и модели из переменных окружения
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-API_TOKEN = os.environ.get('API_TOKEN')
+# устанавливаем токен для взаимодействия с Telegram API
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+# устанавливаем токен для взаимодействия с Replicate API
+REPLICATE_API_TOKEN = 'YOUR_REPLICATE_API_TOKEN'
 
-# Создать экземпляр бота и диспетчер
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher(bot)
+bot = aiogram.Bot(token=BOT_TOKEN)
+dp = aiogram.dispatcher.Dispatcher(bot)
 
-# Функция для вызова модели и генерации изображений
-async def generate_images(prompt):
+# функция генерации изображения на Replicate
+async def generate_image(input_text: str):
+    output = replicate.run(
+       "ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
+        input={"prompt": input_text}
+    )
+    return output[0]  # возвращаем ссылку на сгенерированное изображение
+
+# функция, которая будет вызываться при отправке сообщений боту
+async def get_image(message: types.Message):
     try:
-        # Запустить модель
-        output = replicate.run(
-            "ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
-            input={"prompt": prompt}
-        )
-        # Вернуть URI-адреса сгенерированных изображений в виде строки
-        return '\n'.join(output)
+        # запрашиваем изображение на Replicate
+        image_url = await generate_image(message.text)
+        # отправляем пользователю ссылку на сгенерированное изображение
+        await bot.send_message(message.chat.id, f"Вот ваше изображение: {image_url}")
     except Exception as e:
-        # Обработать ошибки (например, если API модели недоступно)
-        return f'Error: {e}'
+        print(f"Ошибка: {e}")
+        await bot.send_message(message.chat.id, "Извините, произошла ошибка, повторите попытку позднее.")
 
-# Обработчик команды /start
-@dp.command_handler(commands=['start'])
-async def start_handler(message):
-    await bot.send_message(message.chat.id, 'Привет! Я могу сгенерировать изображения на основе твоего текстового запроса.')
-
-# Обработчик команды /generate
-@dp.command_handler(commands=['generate'])
-async def generate_handler(message):
-    # Получить текстовое сообщение от пользователя
-    prompt = message.text.split(' ')[1]
-    # Запустить модель и получить URI-адреса сгенерированных изображений
-    output = await generate_images(prompt)
-    # Отправить изображения пользователю в виде URI-адресов
-    await bot.send_message(message.chat.id, output)
-
-# Запустить бота
-bot.run_forever()
+# запускаем бота
+if __name__ == '__main__':
+    # устанавливаем токен для взаимодействия с Replicate API как переменную среды
+    os.environ['REPLICATE_API_TOKEN'] = REPLICATE_API_TOKEN
+    # создаем обработчик сообщений
+    dp.register_message_handler(get_image, filters=filters.Text)
+    # запускаем бота
+    aiogram.executor.start_polling(dp, skip_updates=True)
